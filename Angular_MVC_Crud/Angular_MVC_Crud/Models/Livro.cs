a5502637f2e987cc47b7ac02398b31a7757dffa7
Gabriel Leal <gabriel.sala.oliveira@gmail.com>
2018-04-17 01:20:59 -0300

    Upload do projeto para o git

---
+++ Angular_MVC_Crud/Angular_MVC_Crud/Models/Livro.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Angular_MVC_Crud.Models
+{
+    [Table("Livros")]
+    public class Livro
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Titulo { get; set; }
+        public string Autor { get; set; }
+    }
+}
